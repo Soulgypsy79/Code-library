18a1f1b0cd75fd27e9981ae472d7a88553e1f6c1
Soulgypsy79 <77301327+Soulgypsy79@users.noreply.github.com>
2021-02-09 18:28:40 +0000

    Add files via upload

---
+++ Scripts/Library.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Library : MonoBehaviour
+{
+    public Spawn SpawnTemplate;
+
+    private void Start()
+    {
+
+
+        // Clone the template game object
+        Spawn ballClone = Instantiate(SpawnTemplate);
+
+        // Move the new ball clone to the ball spawner's position
+        ballClone.transform.position = transform.position;
+
+        // Activate the new ball clone
+        ballClone.gameObject.SetActive(true);
+
+       
+    }
+
+}
+
+
+
+
---
+++ Scripts/Spawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+
+{
+    public float size = 1.0f;
+    float speed = 0.2f;
+    float directionX = -1.0f;
+    
+
+    void FixedUpdate()
+    {
+        Vector3 scale = new Vector3();
+        scale.x = size;
+        scale.y = size;
+        transform.localScale = scale;
+
+        Vector3 position = transform.localPosition;
+        position.x += speed * directionX;
+        transform.localPosition = position;
+    }
+
+
+}
